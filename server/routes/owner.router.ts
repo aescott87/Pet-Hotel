65787114f4c2450298784546cb24363cf30eda3c
HornerJustinT <Horner.Justin.T@gmail.com>
2020-05-14 10:45:14 -0500

    set up routes

---
+++ server/routes/owner.router.ts
@@ -0,0 +1,8 @@
+import { Request, Response } from "express";
+import express from "express";
+import pool from "../modules/pool";
+
+const router: express.Router = express.Router();
+
+
+export default router;
